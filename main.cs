40ee1df9d5df23fd6a5a054b6009548d4dc1e0d1
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-18 17:10:38 +0000

    init

--- main.cs
+++ main.cs
@@ -1,7 +1,18 @@
 using System;
+using System.Threading.Tasks;
+using System.IO;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+  public static async Task Main()
+    {
+        string text =
+            "A class is the most powerful data type in C#. Like a structure, " +
+            "a class defines the data and behavior of the data type. ";
+
+        await File.WriteAllTextAsync("WriteText.txt", text);
+        Console.WriteLine("File Created ...");
+
+        var someText = await File.ReadAllTextAsync(@"WriteText.txt");
+        Console.WriteLine(someText);
+    }
 }
