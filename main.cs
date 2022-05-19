197e7ba3c827edb8a080295fafeae24013dbd05b
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-19 01:20:27 +0000

    changes in main.cs

--- main.cs
+++ main.cs
@@ -1,3 +1,8 @@
+// Created by: Kenny Le
+// Created on: May 2022
+//
+// This program saves to local storage
+
 using System;
 using System.Threading.Tasks;
 using System.IO;
@@ -5,9 +10,20 @@ using System.IO;
 class Program {
   public static async Task Main()
     {
-        string text =
-            "A class is the most powerful data type in C#. Like a structure, " +
-            "a class defines the data and behavior of the data type. ";
+        int yes = 1;
+        string no;
+        int counter = 0;
+        string text;
+      
+        Console.WriteLine("Do you want to quit the program?");
+        Console.WriteLine("");
+        Console.WriteLine("Type 'yes' to end the program or type 'no' to add 1 to the counter");
+        yes = Convert.ToInt32(Console.ReadLine());
+        no = Convert.ToString(Console.ReadLine());
+
+
+        text =
+            "Number: ";
 
         await File.WriteAllTextAsync("WriteText.txt", text);
         Console.WriteLine("File Created ...");
